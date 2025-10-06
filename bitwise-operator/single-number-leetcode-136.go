d27d3599f17441d9b1f8bac634226197720cd7be
FarajGopher <farajtomar@gmail.com>
2025-10-07 00:12:58 +0530

    optimized approach to fin single ement in array using xor

--- bitwise-operator/single-number-leetcode-136.go
+++ bitwise-operator/single-number-leetcode-136.go
@@ -20,9 +20,27 @@ func singleNumber(nums []int) int {
     return -1
 }
 
+// optimized approach using XOR
+//XOR help in finding the unique number in an array where every other number appears twice.
+//XOR of a number with itself is 0 (a ^ a = 0).
+//XOR of a number with 0 is the number itself (a ^ 0 = a).
+//XOR is commutative and associative, meaning the order of operations does not matter.
+func singleNumberXOR(nums []int) int {
+    result := 0
+
+    for _, num := range nums {
+        result ^= num
+    }
+
+    return result
+}
+
 
 func main() {
     nums := []int{4,1,2,1,2}
     result := singleNumber(nums)
-    fmt.Println("The single number is:", result) // Output: The single number is: 4
+    fmt.Println("The single number is:", result)
+    result = singleNumberXOR(nums)
+    fmt.Println("The single number using XOR is:", result)
+
 }
